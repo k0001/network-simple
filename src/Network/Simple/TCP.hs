ac8e841866efec856f3648aa2f95b764c6b9b3a1
Renzo Carbonara <gnuk0001@gmail.com>
2013-06-10 03:01:09 -0300

    Export withSocketsDo

--- src/Network/Simple/TCP.hs
+++ src/Network/Simple/TCP.hs
@@ -30,6 +30,10 @@ module Network.Simple.TCP (
   , bindSock
   , connectSock
 
+  -- * Note to Windows users
+  -- $windows-users
+  , NS.withSocketsDo
+
   -- * Exports
   , HostPreference(..)
   ) where
@@ -73,6 +77,30 @@ import           Network.Simple.Internal
 -- different hosts.
 
 --------------------------------------------------------------------------------
+
+-- $windows-users
+--
+-- If you are running Windows, then you /must/ call 'NS.withSocketsDo', just
+-- once, right at the beginning of your program. That is, change your program's
+-- 'main' function from:
+--
+-- > main = do
+-- >   print "Hello world"
+-- >   -- rest of the program...
+--
+-- To:
+--
+-- > main = withSocketsDo $ do
+-- >   print "Hello world"
+-- >   -- rest of the program...
+--
+-- If you don't do this, your networking code won't work and you will get many
+-- unexpected errors at runtime. If you use an operating system other than
+-- Windows then you don't need to do this, but it is harmless to do it, so it's
+-- recommended that you do, for portability reasons.
+
+--------------------------------------------------------------------------------
+
 
 -- $client-side
 --
